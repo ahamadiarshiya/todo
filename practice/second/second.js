41275e3928ddf6166c62ae393b8aad41b4fe3584
ahamadiarshiya <151557820+ahamadiarshiya@users.noreply.github.com>
2025-07-08 05:52:18 -0700

    Delete practice directory

--- practice/second/second.js
+++
@@ -1,5 +0,0 @@
-// let newbutton=document.createElement("button");
-// newbutton.innerText="click me";
-// newbutton.style.backgroundColor="red";
-// newbutton.style.color="white";
-// document.querySelector("div").append(newbutton);
